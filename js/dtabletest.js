44e8c7db7a22a14135df9ef878430a491e1d6026
Lzeey <zeyi@berkeley.edu>
2016-08-24 22:25:07 +0800

    Datatables stuff

---
+++ js/dtabletest.js
@@ -0,0 +1,32 @@
+function datatables_csv(fileuri, tableid, dtoptions) {
+  if (typeof dtoptions === 'undefined') { dtoptions = {}; }
+ 
+  d3.text(fileuri, function (datasetText) {
+ 
+    var rows = d3.csvParseRows(datasetText);
+ 
+    var tbl = d3.select("#" + tableid);
+ 
+    // headers
+    tbl.append("thead").append("tr")
+      .selectAll("th")
+        .data(rows[0])
+      .enter().append("th")
+        .text(function(d) { return d; });
+ 
+    // data
+    tbl.append("tbody")
+      .selectAll("tr")
+          .data(rows.slice(1))
+      .enter().append("tr")
+        .selectAll("td")
+          .data(function(d){ return d; })
+        .enter().append("td")
+          .text(function(d){ return d; });
+ 
+    jQuery(document).ready(function() {
+      jQuery("#" + tableid).DataTable(dtoptions);
+    });
+ 
+  });
+}
